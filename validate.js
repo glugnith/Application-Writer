3c274561e7ae639ccfc5c1106262a26b3912e6d2
Saurabh Kumar <saurabh.k0402@gmail.com>
2015-09-05 15:29:00 +0530

    Add black border

--- validate.js
+++ validate.js
@@ -10,7 +10,7 @@ function funcValidate(){
     if(Number(arr[0])>31 || Number(arr[1]>12))
         document.getElementById("inputDate").style.border="2px solid red";
     else
-      document.getElementById("inputDate").style.border="none";
+      document.getElementById("inputDate").style.border="1px solid black";
   }
   if(!(/\d\d\:\d\d\:\d\d/.test(inputTime))){
       document.getElementById("inputTime").style.border="2px solid red";
@@ -20,6 +20,6 @@ function funcValidate(){
     if(Number(arr[0])>23 || Number(arr[1]>59) || Number(arr[2]>59))
         document.getElementById("inputTime").style.border="2px solid red";
     else
-      document.getElementById("inputTime").style.border="none";
+      document.getElementById("inputTime").style.border="1px solid black";
   }
 }
